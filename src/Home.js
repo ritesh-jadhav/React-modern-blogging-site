a81def3306a19fec630cc1f50b060f5058d6f5c7
ritesh-jadhav <jadhav.ritesh0416@gmail.com>
2021-06-01 11:26:53 +0530

    conditional success

---
+++ src/Home.js
@@ -0,0 +1,26 @@
+import { useState, useEffect } from 'react';
+import BlogList from './BlogList';
+
+const Home = () => {
+    const [blogs, setBlogs] = useState(null);
+    const[isPending,setIsPanding] = useState(true);
+    useEffect(() => {
+        setTimeout(()=>{
+            fetch(' http://localhost:8000/blogs')
+        .then(res =>{
+            return res.json();
+        }).then(data =>{
+            setBlogs(data);
+            setIsPanding(false);
+        },1000);
+        })
+    }, [] );
+
+    return (
+        <div className="home">
+            {isPending && <div>Loading....</div>}
+            {blogs && <BlogList blogs={blogs} title="All about blogs"  />}
+        </div>
+    );
+}
+export default Home;
